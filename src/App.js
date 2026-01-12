59d7033543f3a67a34ecaa7bb1ce14df388104de
Milan Hirpara <hirparamilan@gmail.com>
2026-01-12 15:35:23 +0530

    Routing and adding news related components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 
-import React, { Component } from 'react'
+import { Component } from 'react'
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello my first class based component
-      </div>
+      <HashRouter>
+        <NavBar />
+        {/* <Alert alert={alert} /> */}
+        {/* <div> */}
+          <Routes>
+            <Route index path="/" element={<News />} />
+            {/* <Route path="/about" element={<About darkMode={darkMode} />} /> */}
+          </Routes>
+        {/* </div> */}
+      </HashRouter>
     )
   }
 }
